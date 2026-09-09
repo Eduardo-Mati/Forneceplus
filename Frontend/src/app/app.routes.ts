68242fd82fc175c61061594b502073df413d60f0
Eduardo Matias <eduuumattias@gmail.com>
2026-09-09 20:35:13 -0300

    Login prototype completed, further link to backend required

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Login } from './login/login';
+import { Cadastro } from './cadastro/cadastro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "home", pathMatch: 'full' },
+    { path: "home", component: Home },
+    { path: "cadastro", component: Cadastro },
+    { path: "login", component: Login },
+];
